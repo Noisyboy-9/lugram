c3ce8e43f9f58542113034c066514dac05c2490b
sina shariati <sina.shariati@yahoo.com>
2020-09-30 22:26:14 +0330

    Update test example file to have a sample test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -7,4 +7,10 @@ use Laravel\Lumen\Testing\DatabaseTransactions;
 class ExampleTest extends TestCase
 {
     use DatabaseTransactions;
+
+    /** @test * */
+    public function sample_test()
+    {
+        $this->assertTrue(true);
+    }
 }
---
+++ tests/Feature/ImageUploadTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppTests\Feature;
+
+use AppTests\TestCase;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class ImageUploadTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test * */
+    public function an_image_can_be_uploaded_and_a_new_post_been_created_with_it()
+    {
+    }
+}

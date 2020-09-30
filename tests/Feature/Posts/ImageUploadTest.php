ee1f88f43a145df49439e8eb506e1b52094f9b41
sina shariati <sina.shariati@yahoo.com>
2020-09-30 22:32:06 +0330

    Add facades and eloquent to project

--- tests/Feature/Posts/ImageUploadTest.php
+++ tests/Feature/Posts/ImageUploadTest.php
@@ -3,6 +3,7 @@
 namespace AppTests\Feature\Posts;
 
 use AppTests\TestCase;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class ImageUploadTest extends TestCase
@@ -12,7 +13,7 @@ class ImageUploadTest extends TestCase
     /** @test * */
     public function an_image_can_be_uploaded_and_a_new_post_been_created_with_it()
     {
-        // given
+        // given we have a storage for saving images and we have a image
 
         // when
 

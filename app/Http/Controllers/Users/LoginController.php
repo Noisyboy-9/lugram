6f7c8532150a93d7852dff46179dcb9dc069e2e8
sina shariati <sina.shariati@yahoo.com>
2020-10-05 00:48:21 +0330

    WIP : auth attempt returns false every tome

--- app/Http/Controllers/Users/LoginController.php
+++ app/Http/Controllers/Users/LoginController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Crypt;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -13,13 +12,7 @@ class LoginController extends Controller
     {
         $attributes = $this->validateLogin($request);
 
-        if ($this->isMember($attributes) && $this->isInputPasswordCorrect($attributes)) {
-            $id = DB::table('users')->where('email', $attributes['email'])->pluck('id')[0];
-
-            return $this->respondWithToken(auth()->tokenById($id));
-        }
-
-        return response()->json(['message' => 'Email and password combination does not match out records!'], 401);
+        dd(Auth::attempt($attributes)); // why this returns false everytime
     }
 
     private function validateLogin(Request $request)
@@ -27,29 +20,6 @@ class LoginController extends Controller
         return $this->validate($request, [
             'email' => 'required|email:rfc',
             'password' => 'required',
-        ]);
-    }
-
-    private function isMember(array $attributes): bool
-    {
-        return DB::table('users')
-            ->where('email', $attributes['email'])
-            ->exists();
-    }
-
-    private function isInputPasswordCorrect(array $attributes)
-    {
-        $hashedPassword = DB::table('users')->pluck('password');
-
-        return $attributes['password'] === Crypt::decrypt($hashedPassword);
-    }
-
-    private function respondWithToken(string $token)
-    {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60,
         ]);
     }
 }

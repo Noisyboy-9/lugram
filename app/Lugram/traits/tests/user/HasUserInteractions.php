c2c7e7c956aa3e89c5c3346c751ef6b4021931c8
sina shariati <sina.shariati@yahoo.com>
2020-10-07 14:00:13 +0330

    Fix problem with createUser and makeUser function
    createUser and makeUser are meant to be in trait HasUserInteractions
    but It was wrongly placed in InteractsWithExceptionHandling trait.
    So changed their position to be in the right trait.

--- app/Lugram/traits/tests/user/HasUserInteractions.php
+++ app/Lugram/traits/tests/user/HasUserInteractions.php
@@ -4,7 +4,50 @@
 namespace App\Lugram\traits\tests\user;
 
 
+use App\Models\User;
+
 trait HasUserInteractions
 {
 
+    /**
+     * create users but don't persist them to database
+     *
+     * @param array $attributes
+     * @param int   $count
+     *
+     * @note this function does not persist data to databsae
+     * @return array|mixed
+     */
+    protected function makeUser(array $attributes = [], int $count = 1)
+    {
+        $users = User::factory()->count($count)->make($attributes);
+
+        foreach ($users as $user) {
+            $user['password_confirmation'] = $user['password'];
+        }
+
+        if ($count === 1) return $users->toArray()[0];
+
+        return $users->toArray();
+    }
+
+    /**
+     * create users and persist them to the  database
+     *
+     * @param array $attributes
+     * @param int   $count
+     *
+     * @note this function persist data to database
+     *
+     * @return array|mixed
+     */
+    protected function createUser(array $attributes = [], int $count = 1)
+    {
+        $users = User::factory()->count($count)->make($attributes);
+        foreach ($users as $user) $user->save();
+
+        if ($count === 1) return $users->toArray()[0];
+
+        return $users->toArray();
+    }
 }
